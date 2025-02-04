e0da7b60fdf1247ccf7341999653a0a1a4f860e4
elza akhmatyanova <akh24071998@yandex.ru>
2025-02-04 18:49:56 +0500

    adding method determineEvenNumbers in App class

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,19 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
+        System.out.println("Please enter the game number and press Enter.\n1 - Greet\n2 - Even\n0 - Exit\n");
+
+        Scanner scanner = new Scanner(System.in);
+
+        Cli.choosingGame(scanner);
         System.out.println("Welcome to the Brain Games!");
-        Cli.welcomeUser();
+        String userName = Cli.gettingName(scanner);
+
+        EvenGame.determineEvenNumbers(scanner, userName);
+
+        scanner.close();
     }
 }
