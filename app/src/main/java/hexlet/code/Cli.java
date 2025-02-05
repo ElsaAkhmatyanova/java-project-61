23d34ee3741d21b61d0067cb0262b3d1712bea9b
elza akhmatyanova <akh24071998@yandex.ru>
2025-02-05 17:39:34 +0500

    reformating choosing game in GamesSettings class

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -3,7 +3,6 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
-    int[] gameNumber = new int[]{0,1,2};
 
     public static String gettingName(Scanner scanner) {
         System.out.println("May I have your name?");
@@ -14,15 +13,6 @@ public class Cli {
         } else {
             System.out.println("No input provided!");
             return null;
-        }
-    }
-
-    public static void choosingGame(Scanner scanner) {
-        String userGameChoice = scanner.nextLine();
-        if (userGameChoice != null) {
-            System.out.println("Your choice: " + userGameChoice);
-        } else {
-            System.out.println("No input provided!");
         }
     }
 }
---
+++ app/src/main/java/hexlet/code/GamesSettings.java
@@ -0,0 +1,29 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class GamesSettings {
+    public static int choosingGame(Scanner scanner) {
+        System.out.println("Please enter the game number and press Enter.\n1 - Greet\n2 - Even\n0 - Exit\n");
+        String userGameChoice = scanner.nextLine();
+        int userGame = Integer.parseInt(userGameChoice);
+
+        int[] listOfGames = new int[]{1, 2};
+        boolean isSelectedExistedGame = false;
+
+        for (int i : listOfGames) {
+            if (i == userGame) {
+                isSelectedExistedGame = true;
+                break;
+            }
+        }
+
+        if (isSelectedExistedGame) {
+            System.out.println("Your choice: " + userGameChoice);
+            return Integer.parseInt(userGameChoice);
+        } else {
+            System.out.println("No input provided!");
+            return 0;
+        }
+    }
+}
