1ee28606c059331997d5c6369f8a33a82f3f5ed9
elza akhmatyanova <akh24071998@yandex.ru>
2025-02-05 23:00:46 +0500

    adding CalcGame class

---
+++ app/src/main/java/hexlet/code/games/CalcGame.java
@@ -0,0 +1,13 @@
+package hexlet.code.games;
+
+import java.util.Scanner;
+
+public class CalcGame {
+    public static void calcNumbers(Scanner scanner, String userName) {
+        System.out.println("What is the result of the expression?");
+
+
+
+
+    }
+}
