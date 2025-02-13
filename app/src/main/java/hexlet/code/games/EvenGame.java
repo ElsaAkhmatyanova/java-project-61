8ea9de39378dc9f1c1a7389e7a81815b78fce267
elza akhmatyanova <akh24071998@yandex.ru>
2025-02-13 19:03:41 +0500

    fixing game logic

--- app/src/main/java/hexlet/code/games/EvenGame.java
+++ app/src/main/java/hexlet/code/games/EvenGame.java
@@ -1,24 +1,24 @@
 package hexlet.code.games;
 
 import hexlet.code.Engine;
-
-import java.util.Random;
+import hexlet.code.common.Constants;
+import hexlet.code.common.Utils;
 
 public class EvenGame {
     public static final String EVEN_MAIN_QUESTION = "Answer 'yes' if the number is even, otherwise answer 'no'.";
 
-    public static String[][] generateEvenGameData() {
-        String[][] gameData = new String[Engine.NUMBER_OF_LEVELS][2];
-        Random randomNumeric = new Random();
+    public static void launchingEvenGame() {
+        String[][] gameData = new String[Constants.NUMBER_OF_LEVELS][2];
 
-        for (int i = 0; i < Engine.NUMBER_OF_LEVELS; i++) {
-            int number = randomNumeric.nextInt(Engine.RANDOM_NUMBER_RANGE);
+        for (int i = 0; i < Constants.NUMBER_OF_LEVELS; i++) {
+            int number = Utils.generateDefaultRandomNumber();
             String question = String.valueOf(number);
             String correctAnswer = (number % 2 == 0) ? "yes" : "no";
 
             gameData[i][0] = question;
             gameData[i][1] = correctAnswer;
         }
-        return gameData;
+
+        Engine.runGame(EVEN_MAIN_QUESTION, gameData);
     }
 }
